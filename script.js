626bd983727719d09b3a494688c6009150e5c20c
Yuki Tatsumi <keplokfor@gmail.com>
2023-07-30 17:17:48 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// This is where you can add some interactivity to your site if needed.
+// For example, let's add a simple greeting to the console.
+
+console.log("Welcome to my professional website!");
